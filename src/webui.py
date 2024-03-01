39c178691c69e6a89f9f11d0d65b0aeb304f8143
Marc Urben <aegnor@mittelerde.ch>
2024-03-01 21:51:38 +0100

    Update stuff

--- src/webui.py
+++ src/webui.py
@@ -76,4 +76,4 @@ if __name__ == '__main__':
     pxc.mqtt_client.loop_start()
     pxc.logger.info(f"Starting flask app in {app.root_path}")
 
-    app.run(debug=True, host="0.0.0.0", port=80)
+    app.run(debug=True, host="0.0.0.0")
