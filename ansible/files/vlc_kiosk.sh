fb3f45883bffe84cb90b82abf5576b02fb76508a
Marc Urben <aegnor@mittelerde.ch>
2024-03-01 20:45:37 +0100

    Working on autostart stuff

--- ansible/files/vlc_kiosk.sh
+++ ansible/files/vlc_kiosk.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/usr/bin/vlc --fullscreen --no-video-title-show --quiet-synchro --no-qt-fs-controller --disable-screensaver -I oldrc --rc-unix /tmp/vlc_rc.sock
+while true;
+do
+  echo "$(date) Starting VLC"
+  /usr/bin/vlc --fullscreen --no-video-title-show --quiet-synchro --no-qt-fs-controller --disable-screensaver -I oldrc --rc-unix /tmp/vlc_rc.sock
+  echo "$(date) VLC ended. Restarting it in 1 second."
+  sleep 1
+done
